4859c767098181780989f241dc5d43708541dba0
Abdullah <mr3bodx123@gmail.com>
2025-11-09 14:14:46 +0300

    done

--- companyproj/compapp/views.py
+++ companyproj/compapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from .models import Employee
+from .models import Employee, Project
 
 # Create your views here.
 
 def employee_list(request):
     employees = Employee.objects.all().order_by('name')
     return render(request, 'employee_list.html', {'employees': employees})
+
+def employee_detail(request, employee_id):
+    employee = Employee.objects.get(id=employee_id)
+    projects = Project.objects.all()
+    return render(request, 'employee_detail.html', {'employee':employee,'projects':projects})
+
